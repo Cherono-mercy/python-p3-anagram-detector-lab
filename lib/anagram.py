89dc1d04de1c6c19b2a8952184c59b4ca5bb6e2d
Cherono-mercy <cheronom142@gmail.com>
2023-11-30 14:20:09 +0800

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+
+# Defining class
+class Anagram:
+    
+    def __init__(self, word):
+        self.word_letters = sorted([letter for letter in word])
+
+    def match(self, match_list):
+        match_word_list = [] 
+
+        for word in match_list:
+            if sorted([letter for letter in word]) == self.word_letters:
+                match_word_list.append(word)
+
+        return match_word_list        
+
